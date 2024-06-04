4b694c8af06625d0d8f5b712b7d636b3592bec55
Rolex <167498105+Sunnykumar1122@users.noreply.github.com>
2024-06-04 18:59:56 +0530

    Update global.py

--- Zaid/modules/globals/global.py
+++ Zaid/modules/globals/global.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = [7037832227, 5959308905]
+DEVS = []
 from Zaid.helper.PyroHelpers import get_ub_chats
 from Zaid.modules.basic.profile import extract_user, extract_user_and_reason
 from Zaid.database import gbandb as Zaid
