fced3179db1f5fb88565550e67dd72cf566d9713
Rolex <167498105+Sunnykumar1122@users.noreply.github.com>
2024-06-04 19:20:42 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "⚡️𝗭𝗘𝗨𝗦⚡️"
+            ac.title = "⚡️ROLEX⚡️"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @ig_synax × @sagebotxsupports •",
+                f"```{str(ac)}```\n• @Fushii_warr × @babe_bot_update •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @ig_synax × @sagebotxsupports •"
+            message, f"```{str(ac)}```\n• @Fushii_warr × @babe_bot_update •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @ig_synax"
+            this_command += "© @Fushii_warr"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
