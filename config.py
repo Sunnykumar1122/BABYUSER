8fb5ed00cd92a8da2b144460f473d970cfe17d54
Rolex <167498105+Sunnykumar1122@users.noreply.github.com>
2024-06-04 12:34:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/6e8bb5bb7cd3a4a534683.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph//file/a9e8e650ac5b49d504902.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/Sagexdd/USERBOT2")
+REPO_URL = getenv("REPO_URL", "https://github.com/Sunnykumar1122/BABYUSER")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
